754185b1925bdfc92485a968604160dca068f969
Aditya Chavan <adityachavan198@gmail.com>
2019-08-20 14:09:50 +0530

    Models created
    ChargingStation model created
    Database online hostinger added

--- ChargeSecure/charge/models.py
+++ ChargeSecure/charge/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ChargeStation(models.Model):
+    latitude = models.CharField(max_length=300)
+    longitude = models.CharField(max_length=300)
