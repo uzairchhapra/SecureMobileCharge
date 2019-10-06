3c2d6b9169a658dcb5b913c6bc8e5819f9c7a08b
sagarambilpure <37987124+sagarambilpure@users.noreply.github.com>
2019-10-06 20:54:02 +0530

    chummaaaaa

--- ChargeSecure/charge/admin.py
+++ ChargeSecure/charge/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
 class ChargeStationAdmin(admin.ModelAdmin):
-    list_display = ['latitude', 'longitude']
+    list_display = ['id','latitude', 'longitude']
+    search_fields=['name','description','latitude','longitude']
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=['id','first_name','last_name','email']
+
 
 
 admin.site.register(ChargeStation,ChargeStationAdmin)
+
+
+# Register your models here.
+admin.site.register(UserOfApp,UserAdmin)
+# admin.site.register(LogOfApp)
+
+admin.site.site_header = "ChargingBuddy Admin"
+admin.site.site_title = "Admin Page"
+admin.site.index_title = "ChargingBuddy Admin Page"
+
--- ChargeSecure/charge/views copy.py
+++ ChargeSecure/charge/views copy.py
@@ -35,6 +35,7 @@ port = 8883 # When no port override for non-WebSocket, default to 8883
 useWebsocket = False
 clientId = 'server'
 topic = 'pub'
+mess='from_server'
 
 # Configure logging
 logger = logging.getLogger("AWSIoTPythonSDK.core")
@@ -86,48 +87,48 @@ def getlocation(request):
 
 def book_a_locker(request):
     global loopcount
-    # host = 'a1wlltnsvntckz-ats.iot.ap-south-1.amazonaws.com'
-    # rootCAPath1 = 'C:/Users/Sameer/Documents/GitHub/SecureMobileCharge/ChargeSecure/charge/certificates/server/root-CA.pem'
-    # certificatePath1 = 'C:/Users/Sameer/Documents/GitHub/SecureMobileCharge/ChargeSecure/charge/certificates/server/1e5e1bc664-certificate.pem.crt'
-    # privateKeyPath1 = 'C:/Users/Sameer/Documents/GitHub/SecureMobileCharge/ChargeSecure/charge/certificates/server/1e5e1bc664-private.pem.key'
-    # port = 8883 # When no port override for non-WebSocket, default to 8883
-    # #port = 443 # When no port override for WebSocket, default to 443
+    host = 'a1wlltnsvntckz-ats.iot.ap-south-1.amazonaws.com'
+    rootCAPath1 = 'C:/Users/Sameer/Documents/GitHub/SecureMobileCharge/ChargeSecure/charge/certificates/dev1/root-CA.pem'
+    certificatePath1 = 'C:/Users/Sameer/Documents/GitHub/SecureMobileCharge/ChargeSecure/charge/certificates/dev1/1e5e1bc664-certificate.pem.crt'
+    privateKeyPath1 = 'C:/Users/Sameer/Documents/GitHub/SecureMobileCharge/ChargeSecure/charge/certificates/dev1/1e5e1bc664-private.pem.key'
+    port = 8883 # When no port override for non-WebSocket, default to 8883
+    #port = 443 # When no port override for WebSocket, default to 443
 
-    # clientId1 = 'server'
+    clientId1 = 'chargebuddy'
     topic = 'pub'
     mess='from_server'
 
-    # # Configure logging
-    # logger1 = logging.getLogger("AWSIoTPythonSDK.core")
-    # logger1.setLevel(logging.DEBUG)
-    # streamHandler = logging.StreamHandler()
-    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # streamHandler.setFormatter(formatter)
-    # logger1.addHandler(streamHandler)
+    # Configure logging
+    logger1 = logging.getLogger("AWSIoTPythonSDK.core")
+    logger1.setLevel(logging.DEBUG)
+    streamHandler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    streamHandler.setFormatter(formatter)
+    logger1.addHandler(streamHandler)
 
-    # # Init AWSIoTMQTTClient
-    # myAWSIoTMQTTClient1 = None
+    # Init AWSIoTMQTTClient
+    myAWSIoTMQTTClient1 = None
     
-    # myAWSIoTMQTTClient1 = AWSIoTMQTTClient(clientId1)
-    # myAWSIoTMQTTClient1.configureEndpoint(host, port)
-    # myAWSIoTMQTTClient1.configureCredentials(rootCAPath1, privateKeyPath1, certificatePath1)
+    myAWSIoTMQTTClient1 = AWSIoTMQTTClient(clientId1)
+    myAWSIoTMQTTClient1.configureEndpoint(host, port)
+    myAWSIoTMQTTClient1.configureCredentials(rootCAPath1, privateKeyPath1, certificatePath1)
     
-    # # AWSIoTMQTTClient connection configuration
-    # myAWSIoTMQTTClient1.configureAutoReconnectBackoffTime(1, 1, 2)
-    # myAWSIoTMQTTClient1.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-    # myAWSIoTMQTTClient1.configureDrainingFrequency(5)  # Draining: 2 Hz
-    # myAWSIoTMQTTClient1.configureConnectDisconnectTimeout(10)  # 10 sec
-    # myAWSIoTMQTTClient1.configureMQTTOperationTimeout(5)  # 5 sec
+    # AWSIoTMQTTClient connection configuration
+    myAWSIoTMQTTClient1.configureAutoReconnectBackoffTime(1, 1, 2)
+    myAWSIoTMQTTClient1.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    myAWSIoTMQTTClient1.configureDrainingFrequency(5)  # Draining: 2 Hz
+    myAWSIoTMQTTClient1.configureConnectDisconnectTimeout(10)  # 10 sec
+    myAWSIoTMQTTClient1.configureMQTTOperationTimeout(5)  # 5 sec
 
 
-    # # Connect and subscribe to AWS IoT
-    # myAWSIoTMQTTClient1.connect()
+    # Connect and subscribe to AWS IoT
+    myAWSIoTMQTTClient1.connect()
     loopcount+=1
     message = {}
     message['message'] = mess
     message['sequence'] = loopcount
     messageJson = json.dumps(message)
-    wow = myAWSIoTMQTTClient.publish(topic, messageJson, 1)
-    # myAWSIoTMQTTClient.disconnect()
+    wow = myAWSIoTMQTTClient1.publish(topic, messageJson, 1)
+    myAWSIoTMQTTClient1.disconnect()
 
     return HttpResponse('Published topic %s: %s\n%s' % (topic, messageJson,str(wow)))
