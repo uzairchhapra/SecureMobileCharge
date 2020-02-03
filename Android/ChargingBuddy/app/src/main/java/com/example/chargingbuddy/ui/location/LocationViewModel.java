5075fa5dbadb85caa5e218f915a05996f8d8684f
sagarambilpure <37987124+sagarambilpure@users.noreply.github.com>
2020-02-04 00:43:01 +0530

    deleted android apps

--- Android/ChargingBuddy/app/src/main/java/com/example/chargingbuddy/ui/location/LocationViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.chargingbuddy.ui.location;
-
-import androidx.lifecycle.ViewModel;
-
-public class LocationViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- Android/KotlinChargingBuddy/app/src/main/java/com/example/kotlinchargingbuddy/ui/location/LocationViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinchargingbuddy.ui.location
-
-import androidx.lifecycle.ViewModel
-
-class LocationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
